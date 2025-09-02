a10d22183694a4adcd27bef467955133040df1dd
Dean Kinyua <kinyuadean@gmail.com>
2025-09-02 09:16:12 +0300

    use forEach loop to loop over array elements

---
+++ objects/array_looping.js
@@ -0,0 +1,33 @@
+let JOURNAL = [
+  {
+    squirrel: false,
+    events: ["Smoking weed", "Cutting firewood", "Cooking tea"],
+  },
+  {
+    squirrel: true,
+    events: ["Go to the shop", "Milk the camel", "Smoking weed"],
+  },
+];
+
+// You can loop like this or use the forEach function which is easier
+
+// for (let entry of JOURNAL) {
+//   console.log(`${entry.events.length} events.`);
+// }
+
+JOURNAL.forEach((entry) => console.log(`${entry.events.length} events.`));
+
+function journalEvents(journal) {
+  let events = [];
+  journal.forEach((entry) => {
+    entry.events.forEach((event) => {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    });
+  });
+
+  return events;
+}
+
+console.log(journalEvents(JOURNAL));
--- objects/arrays.js
+++ objects/arrays.js
@@ -1,8 +1,9 @@
 let list = [1, 2, 3, 4];
 
 let person = {
-  name: "Dean Kinyua",
   age: 22,
+  name: "Dean Kinyua",
+  subjects: ["Maths", "English", "Kiswahili"],
 };
 
 console.log(list[0]);
@@ -14,3 +15,6 @@ let sequence = [1, 2, 3];
 sequence.push(4);
 sequence.push(5);
 console.log(sequence);
+
+// includes method of an array
+console.log(person.subjects.includes("English"));
