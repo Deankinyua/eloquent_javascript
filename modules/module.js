1a5dfdd2a1ba6e56f77e956d3c53e84d877a5082
Dean Kinyua <kinyuadean@gmail.com>
2025-09-07 21:18:09 +0300

    there can be only one default export per file but multiple named exports

--- modules/module.js
+++ modules/module.js
@@ -1,13 +1,15 @@
 import { create, createReportList } from "canvas";
 import { name, draw, reportArea, reportPerimeter } from "./module/square.js";
-import randomSquare from "./module/square.js";
+import person from "./module/default.js";
+
+console.log(person.body.ears);
 
 let myCanvas = create("myCanvas", document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
 
 let square1 = draw(myCanvas.ctx, 50, 50, 100, "blue");
-reportArea(square1.length, reportList);
-reportPerimeter(square1.length, reportList);
+// reportArea(square1.length, reportList);
+// reportPerimeter(square1.length, reportList);
 
 // Use the default
 let square2 = randomSquare(myCanvas.ctx);
---
+++ modules/module/default.js
@@ -0,0 +1,10 @@
+let person = {};
+
+person.body = {
+  ears: 2,
+  mouth: 1,
+  nose: 1,
+};
+
+// default exports 1 per file
+export default person;
