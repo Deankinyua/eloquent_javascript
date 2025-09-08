eac73a6d44aa52df9bb4e83dca1bf53cb59fe6c4
Dean Kinyua <kinyuadean@gmail.com>
2025-09-08 15:09:38 +0300

    write range function

--- range/range.js
+++ range/range.js
@@ -1,12 +1,23 @@
-function range(first_num, second_num) {
+// ? I will shorten this later ... too verbose
+
+const range = (first_num, second_num, step = 1) => {
   let array = [];
   let holder = first_num;
-  while (holder <= second_num) {
-    array.push(holder);
-    holder++;
+
+  if (first_num < second_num) {
+    while (holder <= second_num) {
+      array.push(holder);
+      holder += step;
+    }
+  } else {
+    while (holder >= second_num) {
+      array.push(holder);
+      holder += step;
+    }
   }
+
   return array;
-}
+};
 
 const sum_array = (...numbers) => {
   let sum = 0;
@@ -19,8 +30,10 @@ const sum_array = (...numbers) => {
 };
 
 let new_array = range(2, 5);
-let newer_array = range(0, 15);
+let newer_array = range(10, 5, -1);
+let newest_array = range(-3, -15, -2);
+
 console.log(new_array);
 console.log(newer_array);
-
+console.log(newest_array);
 console.log(sum_array(88, 20, 2));
