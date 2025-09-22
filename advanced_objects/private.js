f9eea6f11be7236a75f3ec721b6503cb47812f98
Dean Kinyua <kinyuadean@gmail.com>
2025-09-22 08:28:33 +0300

    you have to declare private properties

--- advanced_objects/private.js
+++ advanced_objects/private.js
@@ -13,9 +13,10 @@ class User {
     return this.#insultUser();
   }
 
+  // private method
   #insultUser() {
     // Basically hide how insultUser is implemented
-    // Think of private functions as kinda important and you need to
+    // Think of private method as kinda important and you need to
     // hide how you implement them
     return `You are such a bastard ${this.username}`;
   }
@@ -27,17 +28,19 @@ console.log(userOne.sayHi());
 console.log(userOne.response());
 
 class RandomSource {
+  // private properties have to be declared
   #max;
   constructor(max) {
     this.#max = max;
   }
   getNumber() {
-    let randomNum = Math.random()
-    console.log(randomNum)
-    // Using the private method #max
+    let randomNum = Math.random();
+    console.log(randomNum);
+    // Using the private property #max
     return Math.floor(randomNum * this.#max);
   }
 }
 
-const randNum = new RandomSource(90)
-console.log(randNum.getNumber())
+const randNum = new RandomSource(90);
+console.log(randNum.getNumber());
+// console.log([1, 2].toString());
