4e892e8c7246170cf03ace9cc41f6cc7fe819ce1
Dean Kinyua <kinyuadean@gmail.com>
2025-09-25 21:42:16 +0300

    you can add the same symbol to different data structures and it won't conflict with existing properties

--- symbols/iterator.js
+++ symbols/iterator.js
@@ -9,3 +9,14 @@ console.log(nameIterator.next());
 console.log(nameIterator.next());
 console.log(nameIterator.next());
 console.log(nameIterator.next());
+
+// You can now see the advantages of using Symbols
+// We can add the same behaviour to different objects without
+// it conflicting with the other properties of that specific object be it a
+// an array, string and so on 
+let arrayIterator = ["stop", "using", "women"][Symbol.iterator]();
+
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
+console.log(arrayIterator.next());
