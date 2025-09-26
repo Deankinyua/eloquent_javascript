379e8b172e6b05b8c39a24c468b7af757b8447cc
Dean Kinyua <kinyuadean@gmail.com>
2025-09-26 12:07:55 +0300

    create a list iterable

--- symbols/iterator.js
+++ symbols/iterator.js
@@ -13,10 +13,60 @@ console.log(nameIterator.next());
 // You can now see the advantages of using Symbols
 // We can add the same behaviour to different objects without
 // it conflicting with the other properties of that specific object be it a
-// an array, string and so on 
+// an array, string and so on
 let arrayIterator = ["stop", "using", "women"][Symbol.iterator]();
 
 console.log(arrayIterator.next());
 console.log(arrayIterator.next());
 console.log(arrayIterator.next());
 console.log(arrayIterator.next());
+
+// Iterable Linked list
+class List {
+  constructor(value, rest) {
+    // the value stored at this node
+    this.value = value;
+    // Pointer to the next node (another list or null)
+    this.rest = rest;
+  }
+
+  get length() {
+    return 1 + (this.rest ? this.rest.length : 0);
+  }
+
+  static fromArray(array) {
+    let result = null;
+    // Looping from the end of the array, rather than the start
+    for (let i = array.length - 1; i >= 0; i--) {
+      // this, in a static method, points at the constructor
+      // of the class, not an instance
+      result = new this(array[i], result);
+    }
+    return result;
+  }
+}
+
+class ListIterator {
+  constructor(list) {
+    this.list = list;
+  }
+
+  next() {
+    if (this.list == null) {
+      return { done: true };
+    }
+    let value = this.list.value;
+    this.list = this.list.rest;
+    return { value, done: false };
+  }
+}
+
+List.prototype[Symbol.iterator] = function () {
+  return new ListIterator(this);
+};
+
+let list = List.fromArray([1, 2, 3]);
+console.log(list);
+// for (let element of list) {
+//   console.log(element);
+// }
