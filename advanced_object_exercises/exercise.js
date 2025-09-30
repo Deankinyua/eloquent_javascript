08f31ee104b60d4392e8ab9f270cc1bba2ad0b3f
Dean Kinyua <kinyuadean@gmail.com>
2025-09-30 13:53:47 +0300

    add new class called Group which is akin to the built-in type called Set. Make Group iterable by adding a [Symbol.iterator]() method

--- advanced_object_exercises/exercise.js
+++ advanced_object_exercises/exercise.js
@@ -1,4 +1,4 @@
-// The vector type
+// * The vector type
 
 class Vec {
   constructor(x, y) {
@@ -23,8 +23,6 @@ class Vec {
   length() {
     return Math.sqrt(this.x ** 2 + this.y ** 2);
   }
-
-  //   minus(vec) {}
 }
 
 const vec = new Vec(9, 10);
@@ -35,3 +33,78 @@ console.log(vec.plus(newVec));
 console.log(vec.minus(newVec));
 console.log(thirdVec.length());
 console.log(new Vec(9, 12).length());
+
+console.log(NaN !== NaN);
+
+// * The Group type which should behave like a set
+
+const newSet = new Set([1, 2, 4]);
+newSet.add(8);
+console.log(newSet);
+// console.log(newSet.has(8));
+
+class Group {
+  #members = [];
+
+  add(value) {
+    if (!this.has(value)) {
+      this.#members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.#members = this.#members.filter((v) => v !== value);
+  }
+
+  has(value) {
+    return this.#members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group();
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+
+  // Making a Group iterable
+  // Every group iterator must impelement the [Symbol.iterator]() method
+
+  [Symbol.iterator]() {
+    return new GroupIterator(this.#members);
+  }
+}
+
+class GroupIterator {
+  #members;
+  #position;
+
+  constructor(members) {
+    this.#members = members;
+    this.#position = 0;
+  }
+
+  next() {
+    // checks if we are at the last element
+    if (this.#position >= this.#members.length) {
+      return { done: true };
+    } else {
+      // value is the element at the given index
+      let result = { value: this.#members[this.#position], done: false };
+      this.#position++;
+      return result;
+    }
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add("10");
+group.delete(10);
+console.log(group.has(10));
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
