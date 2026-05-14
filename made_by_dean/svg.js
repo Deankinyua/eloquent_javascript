05cbcda2dda44e19e78fc2797addc82020aa3ebe
Dean Kinyua <kinyuadean@gmail.com>
2026-05-14 10:01:34 +0300

    refactor to clean up the body animation

--- made_by_dean/svg.js
+++ made_by_dean/svg.js
@@ -1,5 +1,6 @@
 import animateCartoonHead from "./head_animation.js";
 import { getProgress, getCX } from "./helpers.js";
+import { easeInOut, setLineCoordinates } from "./body_animation_helpers.js";
 
 const line_1 = document.querySelector(".line-1");
 const line_2 = document.querySelector(".line-2");
@@ -11,24 +12,23 @@ const face = document.getElementById("cartoon-face");
 const eye_1 = document.getElementById("eye-1");
 const eye_2 = document.getElementById("eye-2");
 
-const easeInOut = (progress) =>
-  (progress *= 2) < 1
-    ? 0.5 * Math.pow(progress, 5)
-    : 0.5 * ((progress -= 2) * Math.pow(progress, 4) + 2);
-
-// Concerned with the head ----------------------
+// Concerned with the head ------------------------------
+let initialHeadElementIndex = 0;
 const startCXValues = [getCX(face), getCX(eye_1), getCX(eye_2)];
 const finalCXValues = [70, 66.5, 73.5];
 const headElementsAnimatingOrder = [face, eye_1, eye_2];
-let initialHeadElementIndex = 0;
-// -------------------------------------------
+// --------------------------------------------------
 
-const time = {
-  start: performance.now(),
-  total: 1500,
-};
-
-let currentlyAnimatingElementIndex = 0;
+// Concerned with the body------------------------------
+let initialBodyElementIndex = 0;
+const bodyElementsAnimatingOrder = [
+  line_5,
+  line_3,
+  line_1,
+  line_2,
+  line_4,
+  line_5,
+];
 
 let line_1_coordinates = { x1: 70, y1: 80, x2: 70, y2: 105 };
 let line_2_coordinates = { x1: 70, y1: 80, x2: 60, y2: 95 };
@@ -36,70 +36,58 @@ let line_3_coordinates = { x1: 70, y1: 105, x2: 60, y2: 120 };
 let line_4_coordinates = { x1: 70, y1: 80, x2: 80, y2: 95 };
 let line_5_coordinates = { x1: 70, y1: 105, x2: 80, y2: 120 };
 
-const line_coordinates = [
+const lineCoordinates = [
   line_5_coordinates,
   line_3_coordinates,
   line_1_coordinates,
   line_2_coordinates,
   line_4_coordinates,
 ];
+// --------------------------------------------
 
-const line_elements = [line_5, line_3, line_1, line_2, line_4, line_5];
-
-let setAnimationCoordinates = (
-  targetElement,
-  targetElementCoordinates,
-  easingFunc,
+const animateCartoonBody = (
+  targetElementIndex,
+  elementsAnimatingOrder,
+  lineCoordinates,
 ) => {
-  let { x1, y1, x2, y2 } = targetElementCoordinates;
+  let index = targetElementIndex;
+  const targetLine = elementsAnimatingOrder[index];
 
-  const x_distance = x2 - x1;
-  const x_point = x1 + easingFunc * x_distance;
-  const y_distance = y2 - y1;
-  const y_point = y1 + easingFunc * y_distance;
+  const time = {
+    start: performance.now(),
+    total: 1500,
+  };
 
-  targetElement.setAttribute("x1", `${x1}`);
-  targetElement.setAttribute("y1", `${y1}`);
-  targetElement.setAttribute("x2", `${x_point}`);
-  targetElement.setAttribute("y2", `${y_point}`);
+  const animateLegs = (now) => {
+    time.elapsed = now - time.start;
+    const progress = getProgress(time);
+    const easing = easeInOut(progress);
+    const coordinates = lineCoordinates[index];
+
+    setLineCoordinates(targetLine, coordinates, easing);
+
+    if (progress < 1) {
+      requestAnimationFrame(animateLegs);
+    } else {
+      index += 1;
+
+      index < 5
+        ? animateCartoonBody(index, elementsAnimatingOrder, lineCoordinates)
+        : animateCartoonHead(
+            initialHeadElementIndex,
+            finalCXValues,
+            headElementsAnimatingOrder,
+            startCXValues,
+            gaussian,
+          );
+    }
+  };
+
+  requestAnimationFrame(animateLegs);
 };
 
-const getEasingFunc = (time, now) => {
-  if (!time.start) time.start = now;
-  time.elapsed = now - time.start;
-  const progress = getProgress(time);
-  const easing = easeInOut(progress);
-
-  return { easing, progress };
-};
-
-const showLineAnimations = (now) => {
-  let { easing, progress } = getEasingFunc(time, now);
-
-  let index = currentlyAnimatingElementIndex;
-
-  const line = line_elements[index];
-  const coordinates = line_coordinates[index];
-
-  setAnimationCoordinates(line, coordinates, easing);
-
-  if (progress < 1) {
-    requestAnimationFrame(showLineAnimations);
-  } else {
-    time.start = null;
-    currentlyAnimatingElementIndex += 1;
-
-    currentlyAnimatingElementIndex < 5
-      ? requestAnimationFrame(showLineAnimations)
-      : animateCartoonHead(
-          initialHeadElementIndex,
-          finalCXValues,
-          headElementsAnimatingOrder,
-          startCXValues,
-          gaussian,
-        );
-  }
-};
-
-// we should have a function animateCartoonBody
-requestAnimationFrame(showLineAnimations);
+animateCartoonBody(
+  initialBodyElementIndex,
+  bodyElementsAnimatingOrder,
+  lineCoordinates,
+);
