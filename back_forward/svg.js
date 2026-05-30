f5cc69fdee452ad16147dab040f35c2c3a2d2c1e
Dean Kinyua <kinyuadean@gmail.com>
2026-05-30 17:23:18 +0300

    back button animation

---
+++ back_forward/svg.js
@@ -0,0 +1,20 @@
+const backIcon = document.getElementById("back-icon");
+
+backIcon.addEventListener("mouseenter", () => {
+  console.log("I just entered the fucking element");
+});
+
+backIcon.addEventListener("mouseleave", () => {
+  console.log("I just left the fucking element");
+});
+
+//  <polygon
+//     id="polygon-2"
+//     points="
+//         22, 10
+//         11, 16
+//         11, 20
+//         22, 26
+//       "
+//     fill="white"
+//   />
